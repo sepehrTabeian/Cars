cf0400df4ce32f5518e422447cdbe58cdb0677ab
Sepehr_Home <s.tabeian@novin.dev>
2023-06-16 00:22:17 +0330

    Initial Commit

---
+++ Cars/CarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarsApp.swift
+//  Cars
+//
+//  Created by Sepehr on 6/16/23.
+//
+
+import SwiftUI
+
+@main
+struct CarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
