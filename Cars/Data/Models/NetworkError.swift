6fcc43145c16afe7deac5bbc347190670cc4eef6
Sepehr_Home <s.tabeian@novin.dev>
2023-06-16 15:24:50 +0330

    feat <NetworkError> : create NetworkError

---
+++ Cars/Data/Models/NetworkError.swift
@@ -0,0 +1,18 @@
+//
+//  NetworkError.swift
+//  Cars
+//
+//  Created by Sepehr on 6/16/23.
+//
+
+import Alamofire
+struct NetworkError: Error {
+    let initialError: AFError
+    let backendError: BackendError
+
+}
+struct BackendError: Codable, Error {
+    var message: String
+    var statusCode : Int
+    
+}
